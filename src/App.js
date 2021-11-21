01d498c46efb6c46ffad224d5336c853557a8626
an4umov <an4umov@gmail.com>
2021-11-21 23:19:15 +0300

    +likesCounter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 
 function App() {
+  const [likes, setLikes] = useState(7)
+
+  function increment(){
+    setLikes(likes + 1)
+  }
+  
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-     
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
