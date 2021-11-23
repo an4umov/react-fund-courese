3f51640fca9f312e1ab8c281845e2f933c7925be
an4umov <an4umov@gmail.com>
2021-11-23 14:00:05 +0300

    +add valueInput

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 function App() {
   const [likes, setLikes] = useState(7)
+  const [value, setValue] = useState('text inside input')
+  
 
   function increment(){
     setLikes(likes + 1)
@@ -16,8 +18,14 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>{value}</h1>
+      <input
+       type="text"
+       value={value}
+       onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment}>Increment +</button>
+      <button onClick={decrement}>Decrement -</button>
     </div>
   );
 }
